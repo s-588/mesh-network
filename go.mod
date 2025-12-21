fad7fe8eb37b1be0c76de9bdb6a317c57ac2c355
s-588 <sergeivanov827@gmail.com>
2025-12-21 20:25:10 +0300

    add tun iface init logic

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/s-588/mesh-network
 
 go 1.25.3
+
+require (
+	github.com/songgao/water v0.0.0-20200317203138-2b4b6d7c09d8 // indirect
+	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,4 +1,83 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/songgao/water"
+	"golang.org/x/sync/errgroup"
+)
+
 func main() {
+	tun := setupTUN()
+	slog.Info("TUN iface created", "name", tun.Name())
+	defer tun.Close()
+	
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	errGroup, ctx := errgroup.WithContext(ctx)
+	errGroup.Go(
+		func() error {
+			return readTUN(ctx, tun)
+		},
+	)
+	msgChan := make(chan []byte, 1)
+	errGroup.Go(
+		func() error {
+			return writeTUN(ctx, tun, msgChan)
+		},
+	)
+
+	if err := errGroup.Wait(); err != nil{
+		if errors.Is(err, context.Canceled){
+		slog.Error("goroutine in errgroup return a error", "error", err)
+		}
+	}
+	slog.Info("shutting down")
+}
+
+func setupTUN() *water.Interface{
+	cfg := water.Config{
+	DeviceType: water.TUN,			
+	}
+	tun, err := water.New(cfg)
+	if err != nil{
+		panic(fmt.Errorf("can't create TUN interface: %v", err))
+	}
+	return tun
+}
+
+func readTUN(ctx context.Context, tun *water.Interface) error{
+	for {
+		select{
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			b := make([]byte,1024)
+			_, err := tun.Read(b)
+			if err != nil{
+				return fmt.Errorf("can't read from TUN iface: %v",err)
+			}
+		}
+	}
+}
+
+func writeTUN(ctx context.Context, tun *water.Interface, msg <- chan []byte) error{
+	for {
+		select{
+		case <-ctx.Done():
+			return ctx.Err()
+		case m := <- msg:
+			_, err := tun.Write(m)
+			if err != nil{
+				return fmt.Errorf("can't write to TUN iface: %v",err)
+			}
+		}
+	}
 }
