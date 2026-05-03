68c3e39bc2cc00c02a185dab6307969070d12f45
s-588 <sergeivanov827@gmail.com>
2026-05-03 17:54:31 +0300

    implements Stringer and Error interfaces for ErrorCode struct

--- internal/protocol/errors.go
+++ internal/protocol/errors.go
@@ -2,12 +2,33 @@ package protocol
 
 type ErrorCode uint8
 
-const(
-	ErrNoRoute ErrorCode = iota + 1
-	ErrTTLExpired
-	ErrNeighborDown
-	ErrRouteExpired
-	ErrBlackhole
-	ErrInvalidDstSeq
-	ErrBroadcastStorm
+const (
+	ErrLinkBreak ErrorCode = iota + 1
+	ErrNoRoute
+	ErrDestUnreachable
+	ErrInvalidSeq
+	ErrProtocolViolation
 )
+
+// String corresponds to fmt.Stringer interface
+func (e ErrorCode) String() string {
+	switch e {
+	case ErrLinkBreak:
+		return "link break"
+	case ErrNoRoute:
+		return "no route to destination"
+	case ErrDestUnreachable:
+		return "destination unreachable"
+	case ErrInvalidSeq:
+		return "sequence number violation"
+	case ErrProtocolViolation:
+		return "protocol violation"
+	default:
+		return "unknown error"
+	}
+}
+
+// Error corresponds to default error interface
+func (e ErrorCode) Error() string {
+	return e.String()
+}
